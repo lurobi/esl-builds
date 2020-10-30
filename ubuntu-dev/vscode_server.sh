ed5018cc0cfded406d604c552135c0ee303ed31b
Luke Robison <lukerobison@gmail.com>
2020-10-30 09:14:20 -0500

    Initial Commit

---
+++ ubuntu-dev/vscode_server.sh
@@ -0,0 +1,10 @@
+commit_id="latest"
+
+# Download url is: https://update.code.visualstudio.com/commit:${commit_id}/server-linux-x64/stable
+curl -sSL "https://update.code.visualstudio.com/latest/server-linux-x64/stable" -o /tmp/vscode-server-linux-x64.tar.gz
+
+mkdir -p ~/.vscode-server/bin/${commit_id}
+# assume that you upload vscode-server-linux-x64.tar.gz to /tmp dir
+tar zxvf /tmp/vscode-server-linux-x64.tar.gz -C ~/.vscode-server/bin/${commit_id} --strip 1
+touch ~/.vscode-server/bin/${commit_id}/0
+rm -f /tmp/vscode-server-linux-x64.tar.gz
